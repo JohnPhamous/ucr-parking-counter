bb14a5aa0684639975c3787d06fdda3d054769e8
John Pham <johnphammail@gmail.com>
2018-01-20 23:20:29 -0800

    switching to relative path for assets

--- config/index.js
+++ config/index.js
@@ -40,8 +40,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: './static',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
