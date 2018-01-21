a31ef14b1e63145ba7b7803aa9859c1bb8af7fe9
John Pham <johnphammail@gmail.com>
2018-01-20 23:29:06 -0800

    adding analytics

--- src/main.js
+++ src/main.js
@@ -3,16 +3,21 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import * as VueGoogleMaps from 'vue2-google-maps'
+// import * as VueGoogleMaps from 'vue2-google-maps'
+import VueAnalytics from 'vue-analytics'
 
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyCWCo0SLp-4kPOOxJ8707D4h485lzjZDxg',
-    libraries: 'places' // This is required if you use the Autocomplete plugin
-    // OR: libraries: 'places,drawing'
-    // OR: libraries: 'places,drawing,visualization'
-    // (as you require)
-  }
+// Vue.use(VueGoogleMaps, {
+//   load: {
+//     key: 'AIzaSyCWCo0SLp-4kPOOxJ8707D4h485lzjZDxg',
+//     libraries: 'places' // This is required if you use the Autocomplete plugin
+//     // OR: libraries: 'places,drawing'
+//     // OR: libraries: 'places,drawing,visualization'
+//     // (as you require)
+//   }
+// })
+
+Vue.use(VueAnalytics, {
+  id: 'UA-100060744-3'
 })
 
 Vue.config.productionTip = false
